4ec4462475793c36d2a456396276eaad415fb4cf
Patrick <106843867+patrickstaraccount@users.noreply.github.com>
2024-12-30 14:33:03 +0100

    Initial commit

---
+++ src/main/java/net/patrick/timer/Timer.java
@@ -0,0 +1,16 @@
+package net.patrick.timer;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Timer implements ModInitializer {
+	public static final String MOD_ID = "timer";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/patrick/timer/TimerClient.java
@@ -0,0 +1,10 @@
+package net.patrick.timer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
